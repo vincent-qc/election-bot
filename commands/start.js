c1ea64d569567f68c6bdc2ab4aac01d6870a2cd2
= <vincent.qi@hotmail.com>
2021-07-08 14:50:28 -0700

    Update

--- commands/start.js
+++ commands/start.js
@@ -5,6 +5,7 @@ const allowedOperators = ["586024151932731393", "585653754888716290"];
 
 module.exports.run = async (bot, message, args) => {
     if(!allowedOperators.includes(message.author.id)) return message.channel.send("You do not have the perms to operate this bot. If you believe this is a mistake, contact `Crabo_#7498`");
+    if(!message.guild.me.hasPermission("ADMINISTRATOR")) return message.channel.send("An error has occured");
 
     if(args.length < 3) {
         message.channel.send("Not enough args");
