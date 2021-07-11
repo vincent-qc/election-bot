b5972364995e752b82b946cb6ead129cf6f04801
Vincent Qi <vincent.qi@hotmail.com>
2021-07-10 21:18:49 -0700

    Replace placeholder help text with link to README

--- commands/help.js
+++ commands/help.js
@@ -4,7 +4,7 @@ module.exports.run = async (bot, message, args) => {
 
     const embed = new Discord.MessageEmbed()
         .setTitle("Github Documentation Here")
-        .setURL("https://bit.ly/3yt3UpX")
+        .setURL("https://github.com/Crabo-7498/election-bot#how-to-use")
         .addField("Why can't i just the bot?", "For now, Only Ewan and Vincent can operate this bot (as i don't want anyone DDoSing me internally", true);
 
     message.channel.send(embed);
